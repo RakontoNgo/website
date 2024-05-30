1dd3e5dd188233df9db079a9ca260ea859606796
Thomas Barrial <thomas.barrial@gmail.com>
2024-05-30 16:39:49 -0400

    targetBlanckFormLink

--- src/components/offers/OneOffer.tsx
+++ src/components/offers/OneOffer.tsx
@@ -101,6 +101,8 @@ function OneOffer({ offer, link }: { offer: IOffer; link: string }) {
           <div>
             {offer.googleFormLink ? (
               <a
+                rel="noopener noreferrer"
+                target="_blank"
                 className="flex items-center mt-10 text-background justify-center bg-primary border border-primary px-4 py-3 hover:bg-transparent hover:text-primary transform duration-500  font-bold"
                 href={offer.googleFormLink}
               >
