bf7a6dcef43a4c59a46a25102384ac7c7c0efc25
Thomas Barrial <thomas@barrial.fr>
2024-11-18 18:37:19 -0500

    Fix build error

--- src/components/global/text/BasicText.tsx
+++ src/components/global/text/BasicText.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useSelectedLanguagesFromStore } from '@/store/selectedLanguages.slice';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function BasicText({
   contentFr,
@@ -17,22 +17,32 @@ function BasicText({
   const { selectedLanguage } = useSelectedLanguagesFromStore();
   const content = selectedLanguage === 'Fr' ? contentFr : contentEn;
   const [isExpanded, setIsExpanded] = useState(false);
+  const [displayedContent, setDisplayedContent] = useState(content);
 
   const toggleExpand = () => {
     setIsExpanded((prev) => !prev);
   };
-  const displayedContent = isExpanded
-    ? content
-    : content.length > maxChars
-    ? `${content.slice(0, maxChars)}...`
-    : content;
+
+  useEffect(() => {
+    if (isExpanded) {
+      setDisplayedContent(content);
+    } else if (content.length > maxChars) {
+      setDisplayedContent(`${content.slice(0, maxChars)}...`);
+    } else {
+      setDisplayedContent(content);
+    }
+  }, [isExpanded, content, maxChars]);
   return (
     <p
       className={`font-josefin text-[16px] lg:text-[18px] font-light ${className}`}
     >
-      {displayedContent}{' '}
+      {displayedContent}
       {content.length > maxChars && (
-        <button onClick={toggleExpand} className="text-primary underline ml-2">
+        <button
+          type="button"
+          onClick={toggleExpand}
+          className="text-primary underline ml-2"
+        >
           {isExpanded ? 'Show less' : 'Read more'}
         </button>
       )}
