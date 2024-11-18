2de54da99d2beca4a82a7c49f8eade023ae62518
Thomas Barrial <thomas@barrial.fr>
2024-11-18 18:25:53 -0500

    Fix Team

--- src/components/global/text/BasicText.tsx
+++ src/components/global/text/BasicText.tsx
@@ -1,23 +1,41 @@
 'use client';
 
 import { useSelectedLanguagesFromStore } from '@/store/selectedLanguages.slice';
-import React from 'react';
+import React, { useState } from 'react';
 
 function BasicText({
   contentFr,
   contentEn,
   className,
+  maxChars = 800, // Maximum characters for truncation
 }: {
   contentFr: string;
   contentEn: string;
   className?: string;
+  maxChars?: number;
 }) {
   const { selectedLanguage } = useSelectedLanguagesFromStore();
+  const content = selectedLanguage === 'Fr' ? contentFr : contentEn;
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setIsExpanded((prev) => !prev);
+  };
+  const displayedContent = isExpanded
+    ? content
+    : content.length > maxChars
+    ? `${content.slice(0, maxChars)}...`
+    : content;
   return (
     <p
-      className={`font-josefin text-[16px]  lg:text-[18px]  font-light ${className}`}
+      className={`font-josefin text-[16px] lg:text-[18px] font-light ${className}`}
     >
-      {selectedLanguage === 'Fr' ? contentFr : contentEn}
+      {displayedContent}{' '}
+      {content.length > maxChars && (
+        <button onClick={toggleExpand} className="text-primary underline ml-2">
+          {isExpanded ? 'Show less' : 'Read more'}
+        </button>
+      )}
     </p>
   );
 }
